f34f5a07bd3e2e39fcdfe5bfd0a121a108d12428
Istvan <olivaistvan@gmail.com>
2021-11-01 11:44:34 -0300

    Requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function animals() {
+  return species.reduce((prev, current) => {
+    const obj = prev;
+    obj[current.name] = current.residents.length;
+    return obj;
+  }, {});
 }
 
+function countAnimals(animal) {
+  if (!animal) {
+    return animals();
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return species
+    .find((specie) => specie.name === animal.specie).residents
+    .filter((sex) => sex.sex === animal.sex).length;
+}
+// console.log(countAnimals());
 module.exports = countAnimals;
