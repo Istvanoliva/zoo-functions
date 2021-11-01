062c8374458fb494279d04271d20ae56a4ed69d2
Istvan <olivaistvan@gmail.com>
2021-11-01 19:46:05 -0300

    Requisito 06

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((value) => value.age < 18).length;
+  const adult = entrants.filter((value) => value.age >= 18 && value.age < 50).length;
+  const senior = entrants.filter((value) => value.age >= 50).length;
+  const totalEntrants = { child, adult, senior };
+  return totalEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const people = countEntrants(entrants);
+  const childPrice = people.child * prices.child;
+  const adultPrice = people.adult * prices.adult;
+  const seniorPrice = people.senior * prices.senior;
+
+  return childPrice + adultPrice + seniorPrice;
 }
+
+console.log(calculateEntry());
 
 module.exports = { calculateEntry, countEntrants };
