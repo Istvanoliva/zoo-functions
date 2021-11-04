e8ba96992a52c51ab2893db378329406ea7b25a4
Istvan <olivaistvan@gmail.com>
2021-11-04 20:03:01 -0300

    Requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findAnimal = employees.find((person) => person.id === id).responsibleFor[0];
+  const firtsAnimal = species.find((animale) => animale.id === findAnimal).residents;
+  const older = firtsAnimal.sort((a, b) => b.age - a.age)[0];
+
+  return Object.values(older);
 }
+
+// console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = getOldestFromFirstSpecies;
